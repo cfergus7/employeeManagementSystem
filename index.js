87863b2f2ea90e4bca0f8604e6be85e32176a0ac
Chris Ferguson <cfergus7@gmail.com>
2020-06-23 17:57:33 -0500

    frame work for schema

---
+++ index.js
@@ -0,0 +1,43 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: ""
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    // run the start function after the connection is made to prompt the user
+});
+
+
+// Build a command-line application that at a minimum allows the user to:
+
+//   * Add departments, roles, employees
+
+//   * View departments, roles, employees
+
+//   * Update employee roles
+
+// Bonus points if you're able to:
+
+//   * Update employee managers
+
+//   * View employees by manager
+
+//   * Delete departments, roles, and employees
+
+//   * View the total utilized budget of a department -- ie the combined salaries of all employees in that department
